316e796d7213ec66bbb9a0ec95398bd7e22231d9
slb5251-coder <slb5251@gmail.com>
2026-07-16 13:05:36 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@
   const TEST_MODE_KEY = "opscontrol_homologation_mode";
   const TEST_LOG_KEY = "opscontrol_homologation_log";
   const APP_ENV_KEY = "opscontrol_environment";
-  const APP_VERSION = "20260716-v2-interface-1407-restored-1";
+  const APP_VERSION = "20260716-responsive-all-screens-1";
   const fmt = new Intl.NumberFormat("pt-BR", { maximumFractionDigits: 1 });
   const money = new Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" });
 

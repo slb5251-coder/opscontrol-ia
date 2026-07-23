07c2e61d13b8c669d9675a8f66d0b745d4bba6fb
slb5251-coder <slb5251@gmail.com>
2026-07-23 14:14:42 -0300

    Use controlled manual realtime synchronization

--- opscontrol-next/src/components/RealtimeApp.tsx
+++ opscontrol-next/src/components/RealtimeApp.tsx
@@ -4,6 +4,6 @@ import {AppErrorBoundary} from './AppErrorBoundary';
 import {RealtimeIndicator} from './RealtimeIndicator';
 
 export function RealtimeApp(){
- const{version,state,pendingSync,lastEvent}=useRealtimeRefresh();
- return <AppErrorBoundary><div data-realtime-state={state} data-realtime-pending={pendingSync?'true':'false'}><App key={version}/><RealtimeIndicator state={state} pending={pendingSync} lastEvent={lastEvent}/></div></AppErrorBoundary>;
+ const{version,state,pendingSync,lastEvent,syncNow}=useRealtimeRefresh();
+ return <AppErrorBoundary><div data-realtime-state={state} data-realtime-pending={pendingSync?'true':'false'}><App key={version}/><RealtimeIndicator state={state} pending={pendingSync} lastEvent={lastEvent} onSync={syncNow}/></div></AppErrorBoundary>;
 }

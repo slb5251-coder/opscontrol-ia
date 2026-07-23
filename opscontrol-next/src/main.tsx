58a4a380faf7f331e862a70948d3082acdb4780a
slb5251-coder <slb5251@gmail.com>
2026-07-23 12:08:57 -0300

    style(next): load module layouts

--- opscontrol-next/src/main.tsx
+++ opscontrol-next/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 import './auth.css';
+import './modules.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>
